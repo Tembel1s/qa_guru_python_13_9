1a2bf68dfb84341bb35156f69b6bb6226ee886f4
Sergey Melnikov <tembelis@bk.ru>
2024-06-17 23:18:43 +0300

    Added pytest.ini

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,36 +4,26 @@ from selenium.webdriver.chrome.options import Options
 from selene import browser
 
 
-# @pytest.fixture(scope="function", autouse=True)
-# def setup_browser(request):
-#     options = Options()
-#     selenoid_capabilities = {
-#         "browserName": "chrome",
-#         "browserVersion": "100.0",
-#         "selenoid:options": {
-#             "enableVNC": True,
-#             "enableVideo": True
-#         }
-#     }
-#     options.capabilities.update(selenoid_capabilities)
-#     driver = webdriver.Remote(
-#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-#         options=options
-#     )
-#
-#     browser.config.driver = driver
-#     browser.config.base_url = "https://demoqa.com"
-#
-#     yield browser
-#     browser.quit()
-
 @pytest.fixture(scope="function", autouse=True)
-def browser_management():
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
     browser.config.base_url = "https://demoqa.com"
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = "eager"
-    browser.config.driver_options = driver_options
-    yield
+
+    yield browser
     browser.quit()
+
