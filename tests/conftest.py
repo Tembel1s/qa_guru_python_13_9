0c00b21c4699c631d7c63b2772a40d0f01a007ac
Sergey Melnikov <tembelis@bk.ru>
2024-06-17 22:23:15 +0300

    Fixed conftest errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+from selene import Browser, Config, browser
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -21,9 +21,8 @@ def setup_browser(request):
         options=options
     )
 
-    browser = Browser(Config(driver))
-    yield browser
+    browser.config.driver = driver
 
+    yield
     browser.quit()
-
 
