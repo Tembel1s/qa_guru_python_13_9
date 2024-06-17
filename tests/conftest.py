be3c0f9382e87bcddaf6ebcf883954ddd2d2192a
Sergey Melnikov <tembelis@bk.ru>
2024-06-17 22:52:04 +0300

    Fixed conftest errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config, browser
+from selene import browser
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -22,7 +22,13 @@ def setup_browser(request):
     )
 
     browser.config.driver = driver
+    browser.config.base_url = "https://demoqa.com"
+    # browser.config.window_width = 1920
+    # browser.config.window_height = 1080
+    # driver_options = webdriver.ChromeOptions()
+    # driver_options.page_load_strategy = "eager"
+    # browser.config.driver_options = driver_options
 
-    yield
+    yield browser
     browser.quit()
 
