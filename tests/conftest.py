c5a1dcd97b512e8b00687346de3093dc0d9dae7b
Sergey Melnikov <tembelis@bk.ru>
2024-06-18 00:18:49 +0300

    Refactored test_demoqa.py for allure steps

--- tests/conftest.py
+++ tests/conftest.py
@@ -34,13 +34,3 @@ def setup_browser(request):
 
     browser.quit()
 
-# @pytest.fixture(scope="function", autouse=True)
-# def browser_management():
-#     browser.config.base_url = "https://demoqa.com"
-#     browser.config.window_width = 1920
-#     browser.config.window_height = 1080
-#     driver_options = webdriver.ChromeOptions()
-#     driver_options.page_load_strategy = "eager"
-#     browser.config.driver_options = driver_options
-#     yield
-#     browser.quit()
--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -5,26 +5,51 @@ import allure
 def test_registration_form():
     registration_page = RegistrationPage()
 
-    with allure.step("Открываем главную страницу"):
+    with allure.step("Open browser"):
         registration_page.open()
 
-    (
-        registration_page.fill_first_name("Sergei")
-        .fill_second_name("Melnikov")
-        .fill_email("Sergei@Melnikov.com")
-        .select_gender("Male")
-        .fill_phone_number("4815162342")
-        .fill_date_of_birth("July", "1991", "29")
-        .fill_subjects("Computer Science")
-        .fill_hobbies("Sports")
-        .upload_picture("2024-05-12 22.04.05.jpg")
-        .fill_current_address("Novi Sad")
-        .fill_state("Haryana")
-        .fill_city("Karnal")
-        .submit_form()
-    )
 
-    with allure.step("Проверяем корректность данных регистрации"):
+    with allure.step("Fill first name"):
+        registration_page.fill_first_name("Sergei")
+
+    with allure.step("Fill second name"):
+        registration_page.fill_second_name("Melnikov")
+
+    with allure.step("Fill e-mail"):
+        registration_page.fill_email("Sergei@Melnikov.com")
+
+    with allure.step("Select gender"):
+        registration_page.select_gender("Male")
+
+    with allure.step("Fill phone number"):
+        registration_page.fill_phone_number("4815162342")
+
+    with allure.step("Fill date of birth"):
+        registration_page.fill_date_of_birth("July", "1991", "29")
+
+    with allure.step("Fill subjects"):
+        registration_page.fill_subjects("Computer Science")
+
+    with allure.step("Fill hobbies"):
+        registration_page.fill_hobbies("Sports")
+
+    with allure.step("Fill picture"):
+        registration_page.upload_picture("2024-05-12 22.04.05.jpg")
+
+    with allure.step("Fill current address"):
+        registration_page.fill_current_address("Novi Sad")
+
+    with allure.step("Fill state"):
+        registration_page.fill_state("Haryana")
+
+    with allure.step("Fill city"):
+        registration_page.fill_city("Karnal")
+
+    with allure.step("Submit form"):
+        registration_page.submit_form()
+
+
+    with allure.step("Check registered user"):
         registration_page.should_registered_user_info_with(
         "Sergei Melnikov",
         "Sergei@Melnikov.com",
